815359db7a5c4fdf9389b0444b2e9e74de23d755
Nayan Patil <patil.nayan04@gmail.com>
2021-01-12 23:26:17 +0530

    Configured routes and fixed bugs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { GithubService } from 'src/app/services/github.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  user = null;
+  userName: string;
+  error = null;
 
-  ngOnInit(): void {
+  constructor(private github: GithubService, private ref: ChangeDetectorRef) {}
+
+  ngOnInit(): void {}
+
+  handleFindUser(){
+    this.github.getUserDetails(this.userName).subscribe(
+      (user) => {
+        this.user = user;
+        this.error = null;
+        this.ref.detectChanges();
+      },
+      (err) => {
+        this.user = null;
+        this.error = `User not Found : ${err}`;
+        this.ref.detectChanges();
+      })
   }
 
 }
